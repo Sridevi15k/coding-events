8e334d991c3a57ce804d8461ac232f9dcf07a8bb
Sridevi15k <sridevi15k@gmail.com>
2021-06-10 12:42:57 -0500

    add validation annotations in model

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,22 +1,43 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size ;
 import java.util.Objects;
 
 public class Event {
 
         private int id;
         private static int nextId = 1;
+
+        @NotBlank(message = "Name is required.")
+        @Size(min = 3, max = 50, message = "Name must be between 3 an 50 characters.")
         private String name;
+
+        @NotBlank
+        @Size(max = 500, message = "Description too long!.")
         private String description;
 
-        public Event(String name, String description) {
+        @Email(message = "Invalid email. Try again.")
+        private String contactEmail;
+
+        public Event(String name, String description, String contactEmail) {
             this.name = name;
             this.description = description;
+            this.contactEmail = contactEmail;
             this.id = nextId;
             nextId++;
         }
 
-        public int getId() {
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public int getId() {
             return id;
         }
 
